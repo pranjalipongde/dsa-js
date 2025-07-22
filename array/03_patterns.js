0a78c21872cc558de55f402a755f25217d062bf7
pranjalipongde09 <pranjalipongde9@gmail.com>
2025-07-22 12:55:30 +0530

    patterns added

--- array/03_patterns.js
+++ array/03_patterns.js
@@ -3,9 +3,10 @@
 // ****
 // ****
 
-for (let i = 0; i < 4; i++) {
+let n = 6;
+for (let i = 0; i < n; i++) {
   let row = "";
-  for (j = 0; j < 4; j++) {
+  for (j = 0; j < n; j++) {
     row += "*";
   }
   console.log(row);
