6bc92d2c111e406f01a73f1f65c4299614406fc9
pranjalipongde09 <pranjalipongde9@gmail.com>
2025-07-30 18:44:23 +0530

    practiced 1st pattern

--- loops_patterns.js
+++ loops_patterns.js
@@ -5,9 +5,9 @@
 
 let n = 6;
 for (let i = 0; i < n; i++) {
-  let row = "";
-  for (j = 0; j < n; j++) {
-    row += "*";
+  let row = " ";
+  for (let j = 0; j < n; j++) {
+    row += "* ";
   }
   console.log(row);
 }
