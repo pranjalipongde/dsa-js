0d3bc0ca997bfa2926d1d3170ca37ec503d1687e
pranjalipongde09 <pranjalipongde9@gmail.com>
2025-07-30 21:33:30 +0530

    solved some patterns questions

--- loops_patterns.js
+++ loops_patterns.js
@@ -4,9 +4,69 @@
 // ****
 
 let n = 6;
+
 for (let i = 0; i < n; i++) {
   let row = " ";
   for (let j = 0; j < n; j++) {
+    row += "* ";
+  }
+  console.log(row);
+}
+
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+
+for (i = 0; i < n; i++) {
+  let row = " ";
+  for (let j = 0; j <= i; j++) {
+    row += "* ";
+  }
+  console.log(row);
+}
+
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
+
+for (let i = 0; i < n; i++) {
+  let row = " ";
+  for (let j = 0; j <= i; j++) {
+    row += j + 1 + " ";
+  }
+  console.log(row);
+}
+
+//  1
+//  2 2
+//  3 3 3
+//  4 4 4 4
+//  5 5 5 5 5
+//  6 6 6 6 6 6
+
+for (let i = 0; i < n; i++) {
+  let row = " ";
+  for (let j = 0; j <= i; j++) {
+    row += i + 1 + " ";
+  }
+  console.log(row);
+}
+
+//  * * * * * * *
+//  * * * * * *
+//  * * * * *
+//  * * * *
+//  * * *
+//  * *
+//  *
+
+for (let i = 0; i <= n; i++) {
+  let row = " ";
+  for (let j = 0; j < n - i + 1; j++) {
     row += "* ";
   }
   console.log(row);
