f365c1053bfb72de35d701a5af05bac342c131af
pranjalipongde09 <pranjalipongde9@gmail.com>
2025-08-01 19:24:05 +0530

    learned some advacned ansd tricky patterns

--- loops_patterns.js
+++ loops_patterns.js
@@ -71,3 +71,73 @@ for (let i = 0; i <= n; i++) {
   }
   console.log(row);
 }
+
+// 1 2 3 4 5 6
+// 1 2 3 4 5
+// 1 2 3 4
+// 1 2 3
+// 1 2
+// 1
+for (let i = 0; i < n; i++) {
+  let row = "";
+  for (let j = 1; j < n - i + 1; j++) {
+    row += j + " ";
+  }
+  console.log(row);
+}
+
+//        *
+//       ***
+//      *****
+//     *******
+//    *********
+//   ***********
+//  *************
+
+for (let i = 0; i <= n; i++) {
+  let row = "";
+
+  // space
+  for (let j = 0; j < n - i + 1; j++) {
+    row += " ";
+  }
+
+  // stars
+  for (let j = 0; j < 2 * i + 1; j++) {
+    row += "*";
+  }
+
+  // space
+  for (let j = 0; j < n - i + 1; j++) {
+    row += " ";
+  }
+  console.log(row);
+}
+
+//  *************
+//   ***********
+//    *********
+//     *******
+//      *****
+//       ***
+//        *
+
+for (let i = 0; i <= n; i++) {
+  let row = "";
+
+  // space
+  for (let j = 0; j <= i; j++) {
+    row += " ";
+  }
+
+  // stars
+  for (let j = 0; j < 2 * (n - i) + 1; j++) {
+    row += "*";
+  }
+
+  // space
+  for (let j = 0; j <= i; j++) {
+    row += " ";
+  }
+  console.log(row);
+}
