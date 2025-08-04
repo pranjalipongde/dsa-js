aa86f63c124c9787503f5aeb6bd67ef505bcfe03
pranjalipongde09 <pranjalipongde9@gmail.com>
2025-08-04 16:44:48 +0530

    advanced patterns solved

--- loops_patterns.js
+++ loops_patterns.js
@@ -141,3 +141,71 @@ for (let i = 0; i <= n; i++) {
   }
   console.log(row);
 }
+
+//        *
+//       ***
+//      *****
+//     *******
+//    *********
+//   ***********
+//  *************
+//  *************
+//   ***********
+//    *********
+//     *******
+//      *****
+//       ***
+//        *
+
+for (let i = 0; i <= n; i++) {
+  let row = "";
+
+  // space
+  for (let j = 0; j < n - i + 1; j++) {
+    row += " ";
+  }
+
+  // stars
+  for (let j = 0; j < 2 * i + 1; j++) {
+    row += "*";
+  }
+
+  // space
+  for (let j = 0; j < n - i + 1; j++) {
+    row += " ";
+  }
+  console.log(row);
+}
+for (let i = 0; i <= n; i++) {
+  let row = "";
+
+  // space
+  for (let j = 0; j <= i; j++) {
+    row += " ";
+  }
+
+  // stars
+  for (let j = 0; j < 2 * (n - i) + 1; j++) {
+    row += "*";
+  }
+
+  // space
+  for (let j = 0; j <= i; j++) {
+    row += " ";
+  }
+  console.log(row);
+}
+
+for (let i = 0; i <= 2 * n - 1; i++) {
+  let row = "";
+  let stars = i;
+
+  if (i > n) {
+    stars = 2 * n - i;
+  }
+
+  for (let j = 0; j < stars; j++) {
+    row += "* ";
+  }
+  console.log(row);
+}
