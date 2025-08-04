20102a57a4976218b87a2866dbad1d910e28ff87
pranjalipongde09 <pranjalipongde9@gmail.com>
2025-08-04 19:17:22 +0530

    learned patterns

--- loops_patterns.js
+++ loops_patterns.js
@@ -3,7 +3,7 @@
 // ****
 // ****
 
-let n = 6;
+let n = 5;
 
 for (let i = 0; i < n; i++) {
   let row = " ";
@@ -209,3 +209,95 @@ for (let i = 0; i <= 2 * n - 1; i++) {
   }
   console.log(row);
 }
+
+// 1
+// 0 1
+// 1 0 1
+// 0 1 0 1
+// 1 0 1 0 1
+// 0 1 0 1 0 1
+
+let start = 1;
+for (let i = 0; i < n; i++) {
+  let row = "";
+
+  if (i % 2 === 0) {
+    start = 1;
+  } else {
+    start = 0;
+  }
+
+  for (let j = 0; j <= i; j++) {
+    row += start + " ";
+    start = 1 - start;
+  }
+
+  console.log(row);
+}
+
+// 1       1
+// 12     21
+// 123   321
+// 1234 4321
+// 1234554321
+
+let space = 2 * (n - 1);
+for (let i = 0; i <= n; i++) {
+  let row = "";
+
+  //numers
+  for (let j = 1; j <= i; j++) {
+    row += j;
+  }
+
+  //space
+  for (let j = 0; j <= space; j++) {
+    row += " ";
+  }
+
+  //numbers
+  for (let j = i; j >= 1; j--) {
+    row += j;
+  }
+
+  console.log(row);
+  space -= 2;
+}
+
+// 1
+// 2 3
+// 4 5 6
+// 7 8 9 10
+// 11 12 13 14 15
+let num = 1;
+for (let i = 0; i <= n; i++) {
+  let row = "";
+  for (let j = 0; j < i; j++) {
+    row += num + " ";
+    num = num + 1;
+  }
+
+  console.log(row);
+}
+
+// A
+// A B
+// A B C
+// A B C D
+// A B C D E
+for (let i = 0; i <= n; i++) {
+  let row = "";
+  for (let j = 0; j < i; j++) {
+    row += String.fromCharCode(65 + j) + " ";
+  }
+  console.log(row);
+}
+
+for (let i = 0; i <= n; i++) {
+  let row = "";
+  for (let j = 0; j < n - i + 1; j++) {
+    row += String.fromCharCode(65 + j) + " ";
+  }
+  console.log(row);
+}
+// node loops_patterns.js
