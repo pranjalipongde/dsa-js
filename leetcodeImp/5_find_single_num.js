b356d0dfe15ee54efdb367a956050a6308869860
pranjalipongde09 <pranjalipongde9@gmail.com>
2025-11-01 23:22:44 +0530

    added 2nd solution for single number

--- leetcodeImp/5_find_single_num.js
+++ leetcodeImp/5_find_single_num.js
@@ -1,19 +1,27 @@
 function singleNumber(nums) {
-  let hash = {};
+  //   let hash = {};
+
+  //   for (let i = 0; i < nums.length; i++) {
+  //     if (!hash[nums[i]]) {
+  //       hash[nums[i]] = 1;
+  //     } else {
+  //       hash[nums[i]]++;
+  //     }
+  //   }
+
+  //   for (let i = 0; i < nums.length; i++) {
+  //     if (hash[nums[i]] == 1) {
+  //       return nums[i];
+  //     }
+  //   }
+
+  let xor = 0;
 
   for (let i = 0; i < nums.length; i++) {
-    if (!hash[nums[i]]) {
-      hash[nums[i]] = 1;
-    } else {
-      hash[nums[i]]++;
-    }
+    xor = xor ^ nums[i];
   }
 
-  for (let i = 0; i < nums.length; i++) {
-    if (hash[nums[i]] == 1) {
-      return nums[i];
-    }
-  }
+  return xor;
 }
 
 console.log(singleNumber([4, 1, 2, 1, 2]));
